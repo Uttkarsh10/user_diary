845b09eae40668d55360f748fd25edb22c736075
Uttkarsh <uttkarshsaini10@gmail.com>
2023-09-13 17:05:24 +0530

    UI Part Completed

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faInstagram, faGithub} from '@fortawesome/free-brands-svg-icons'
+import styles from './Footer.module.css'
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.footer}>
+      <div style={{marginTop:'5px'}}>Contact Me</div>
+      <div style={{marginTop:'10px'}}>
+        <FontAwesomeIcon icon = {faLinkedin} className={styles['footer-icons']}/>
+        <FontAwesomeIcon icon = {faInstagram} className={styles['footer-icons']}/>
+        <FontAwesomeIcon icon = {faGithub} className={styles['footer-icons']}/>
+      </div>
+      <div style={{marginTop:'5px'}}>Uttkarsh Saini</div>
+    </div>
   )
 }
 
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import styles from './Main.module.css';
 import Header from '../Layout/Header'
+import Footer from '../Layout/Footer'
 import diaryImage from '../../Images/diary-1.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight} from '@fortawesome/free-solid-svg-icons'
 
 function Main() {
   return (
     <div style={{backgroundImage : `url(${diaryImage})`, backgroundRepeat : "no-repeat", backgroundSize : "cover"}} className={styles.main}>
         <Header/>
+
         <div className={styles['main-text']}>My Diary</div>
         <div className={styles['main-subtext']}>Organize your thoughts or record daily events!<br/>
               <span style={{position:"relative", right:"48px"}}>Beacuse Life is better with journals</span>
         </div>
-        <div className={styles['main-link']}>Get Started</div>
+        {/* <div className={styles['main-link']}>Get Started <FontAwesomeIcon icon={"fa-thin fa-arrow-right"} /></div> */}
+        <div className={styles['main-link']}>Get Started <FontAwesomeIcon icon={faArrowRight} /></div>
+
+        <Footer />
     </div>
   )
 }
