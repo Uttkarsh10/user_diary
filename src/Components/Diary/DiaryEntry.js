bfa2d9a5b0c11f7b312d6d55417b4c9c30676055
Uttkarsh <uttkarshsaini10@gmail.com>
2023-09-20 01:05:42 +0530

    Working on conditional rendering

--- src/Components/Diary/DiaryEntry.js
+++ src/Components/Diary/DiaryEntry.js
@@ -1,31 +1,35 @@
 import React, { useState } from 'react'
 import Modal from '../UI/Modal'
 import styles from './DiaryEntry.module.css'
-import DiaryEntries from './DiaryEntries'
+// import DiaryEntries from './DiaryEntries'
 
 function DiaryEntry() {
-  const [state,setState] = useState(false);
+  const [state1,setState] = useState(true);
 
   const clickHandler = () => {
-    setState(!state);
+    setState(!state1);
   }
+
 
   return (
     <div>
-      {!state && 
+      {state1 && 
         <Modal>
-            <div style={{marginBottom:'5px', fontSize:'20px'}}>Title:</div>
-            <input type='text' placeholder='Add Title here' size='50.5' className={styles.input} style={{padding:'8px'}}/>
-            <div style={{marginBottom:'5px', marginTop:'20px', fontSize:'20px'}}>Content:</div>
+            <div className={styles.title}>Title:</div>
+            <input type='text' placeholder='Add Title here' size='50.5' className={styles.input}/>
+            <div className={styles.content}>Content:</div>
             <textarea type='text' rows='15' cols='47' placeholder='Add Content here' className={styles.input} style={{padding:'10px'}}/>
-            {/* <input type='text' placeholder='Add Content here'/> */}
             <div className={styles.text}>
-                <div style={{marginRight:'15px',fontSize:'20px'}} onClick = {clickHandler}>Save</div>
-                <div style={{fontSize:'20px'}} onClick = {clickHandler}>Cancel</div>
+                <div style={{marginRight:'15px',fontSize:'20px'}}>
+                  Save
+                </div>
+                <div style={{fontSize:'20px'}} onClick = {clickHandler}>
+                  Cancel
+                </div>
             </div>
         </Modal>}
 
-        {state && <DiaryEntries/>}
+        {!state1 && <p>You have no entries....</p>} 
     </div>
   )
 }
