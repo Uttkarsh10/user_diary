36b81d9f6eb0205685a7cb02d61f6ca1f2daf4ff
Uttkarsh <uttkarshsaini10@gmail.com>
2023-09-26 23:37:10 +0530

    Working on error of twice component rendering

--- src/Components/Diary/DiaryEntry.js
+++ src/Components/Diary/DiaryEntry.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Modal from '../UI/Modal'
 import styles from './DiaryEntry.module.css'
 // import DiaryEntries from './DiaryEntries'
@@ -6,31 +6,47 @@ import styles from './DiaryEntry.module.css'
 function DiaryEntry() {
   const [state1,setState] = useState(true);
 
+  useEffect(() => {
+    console.log("Hey");
+  })
+  
+
   const clickHandler = () => {
     setState(!state1);
   }
 
 
-  return (
-    <div>
-      {state1 && 
-        <Modal>
-            <div className={styles.title}>Title:</div>
-            <input type='text' placeholder='Add Title here' size='50.5' className={styles.input}/>
-            <div className={styles.content}>Content:</div>
-            <textarea type='text' rows='15' cols='47' placeholder='Add Content here' className={styles.input} style={{padding:'10px'}}/>
-            <div className={styles.text}>
-                <div style={{marginRight:'15px',fontSize:'20px'}}>
-                  Save
-                </div>
-                <div style={{fontSize:'20px'}} onClick = {clickHandler}>
-                  Cancel
-                </div>
-            </div>
-        </Modal>}
+  // return (
+  //   <div>
+  //     {state1 && 
+  //       <div>
+  //         <p>Modal Content</p>
+  //         <div style={{fontSize:'20px'}} onClick = {clickHandler}>
+  //                 Cancel
+  //         </div>
+  //       </div>
+  //       // <Modal>
+  //       //     <div className={styles.title}>Title:</div>
+  //       //     <input type='text' placeholder='Add Title here' size='50.5' className={styles.input}/>
+  //       //     <div className={styles.content}>Content:</div>
+  //       //     <textarea type='text' rows='15' cols='47' placeholder='Add Content here' className={styles.input} style={{padding:'10px'}}/>
+  //       //     <div className={styles.text}>
+  //       //         <div style={{marginRight:'15px',fontSize:'20px'}}>
+  //       //           Save
+  //       //         </div>
+  //       //         <div style={{fontSize:'20px'}} onClick = {clickHandler}>
+  //       //           Cancel
+  //       //         </div>
+  //       //     </div>
+  //       // </Modal>
+  //     }
 
-        {!state1 && <p>You have no entries....</p>} 
-    </div>
+  //       {!state1 && <p>You have no entries....</p>} 
+  //   </div>
+  // )
+
+  return(
+    <div>Hey there</div>
   )
 }
 
